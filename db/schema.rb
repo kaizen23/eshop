09c10d05d0793f799c384a386f7155eebc8bba60
kaizen <neuralbox@op.pl>
2016-08-18 08:02:02 -0700

    rescue_from ActiveRecord::RecordNotFound

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313213324) do
+ActiveRecord::Schema.define(version: 20160313215429) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
